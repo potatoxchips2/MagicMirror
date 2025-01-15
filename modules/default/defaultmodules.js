044d3513a498fc2e25c7e1e5a755bfe2c41be784
potatoxchips2 <123182275+potatoxchips2@users.noreply.github.com>
2025-01-15 14:07:01 -0500

    idkyet

--- modules/default/defaultmodules.js
+++ modules/default/defaultmodules.js
@@ -2,9 +2,7 @@
  * Default Modules List
  * Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
  */
-const defaultModules = ["alert", "calendar", "clock", "compliments", "helloworld", "newsfeed", "updatenotification", "weather"];
-
-/*************** DO NOT EDIT THE LINE BELOW ***************/
+const defaultModules = ["alert", "calendar", "clock", "compliments",]
 if (typeof module !== "undefined") {
 	module.exports = defaultModules;
 }
